8a126da360a1b3a182fda0ce8728b25062de137d
Qihang <qihang.li.7e7@itb.cat>
2024-03-02 20:30:26 +0100

    added rescue object

---
+++ PR1_SaveOcean/Rescue.cs
@@ -0,0 +1,29 @@
+﻿namespace ObjectClasses
+{
+    public class Rescue
+    {
+        private string? code;
+        private string? date;
+        private AAnimal? specie;
+        private int afectationGrade;
+        private string? location;
+
+        public string? Code { get; set; }
+        public string? Date { get; set; }
+        public AAnimal? Specie { get; set; }
+        public int AfectationGrade { get; set; }
+        public string? Location { get; set; }
+
+
+        public Rescue(string code, string date, AAnimal specie, int afectationGrade, string location)
+        {
+            Code = code;
+            Date = date;
+            Specie = specie;
+            AfectationGrade = afectationGrade;
+            Location = location;
+        }
+
+    }
+}
+
