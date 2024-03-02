6d2cdec958e37e84d13d26c01f9a3f14aa312c57
Qihang <qihang.li.7e7@itb.cat>
2024-03-03 00:12:45 +0100

    implemented function to validate user int inputs closes #3

--- ClassMethods/Utility.cs
+++ ClassMethods/Utility.cs
@@ -2,6 +2,9 @@
 {
     public class Utility
     {
-
+        public static bool CheckIntInput(int value, int min, int max)
+        {
+            return value >= min && value <= max;
+        }
     }
 }
