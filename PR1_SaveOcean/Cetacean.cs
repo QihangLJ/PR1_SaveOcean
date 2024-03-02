07aa33eff6e5983ec92cac948b049da4d2e5cd78
Qihang <qihang.li.7e7@itb.cat>
2024-03-02 19:51:30 +0100

    added cetacean object

---
+++ PR1_SaveOcean/Cetacean.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ObjectClasses
+{
+    public class Cetacean : AAnimal
+    {
+        public Cetacean(string name, string superFamily, string species, float weight) :
+            base(name, superFamily, species, weight)
+        { }
+
+        public override int Treatment(int afectation, int value)
+        {
+            double doubleValue = Convert.ToDouble(afectation);
+            return (int) (afectation - Math.Log10(doubleValue) - value);
+        }
+
+    }
+}
+
