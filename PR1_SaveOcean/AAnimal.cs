a0b9012a6a47eea6b1c391e91ab5112d67e8eb76
Qihang <qihang.li.7e7@itb.cat>
2024-03-02 18:47:27 +0100

    firs commit, added AAnimal class

---
+++ PR1_SaveOcean/AAnimal.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ObjectClasses
+{
+    public abstract class AAnimal
+    {
+        protected string? Name { get; set ; }
+        protected string? SuperFamily { get; set; }
+        protected string? Species { get; set; }
+        protected float Weight { get; set; }
+
+
+        protected AAnimal(string name, string superFamily, string species, float weight)
+        {
+            Name = name;
+            SuperFamily = superFamily;
+            Species = species;
+            Weight = weight;
+        }
+
+        public abstract void Treatment();
+    }
+}
